92be04471887d0cc48d9aef049497a2e125e2d19
SteaMMe123@ya.ru <SteaMMe123@ya.ru>
2024-10-30 17:01:52 +0700

    added encoderService

--- ConsoleClient/Program.cs
+++ ConsoleClient/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using EncoderClientApp;
+using Grpc.Net.Client;
+
+
+
+using var channel = GrpcChannel.ForAddress("https://localhost:7090");
+// создаем клиент
+var client = new Encoder.EncoderClient(channel);
+while (true)
+{
+    Console.Write("Введите текст: ");
+    var plainText = Console.ReadLine();
+    Console.Write("Введите ключ: ");
+    int key;
+    try
+    {
+        key = Convert.ToInt32(Console.ReadLine());
+    }
+    catch (FormatException ex)
+    {
+        Console.WriteLine(ex.ToString());
+        Console.WriteLine("Введите коректное число!!!");
+        return;
+    }
+
+    // обмениваемся сообщениями с сервером
+    var reply = await client.EncryptAsync(new TextForEncrypt { PlainText = plainText, Key = key });
+    Console.WriteLine($"Ответ сервера: {reply.Response}");
+    Console.ReadKey();
+}
+
---
+++ GrpcService1/Services/EncoderService.cs
@@ -0,0 +1,46 @@
+﻿using Grpc.Core;
+using GrpcServiceServer;
+using Microsoft.AspNetCore.DataProtection.KeyManagement;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace GrpcServiceServer.Services
+{
+    public class EncoderService : Encoder.EncoderBase
+    {
+        private readonly ILogger<EncoderService> _logger;
+        public EncoderService(ILogger<EncoderService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task<EncryptText> Encrypt(TextForEncrypt request, ServerCallContext context)
+        {
+            //символы русской азбуки
+            const string alfabet = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ";
+            //добавляем в алфавит маленькие буквы
+            var fullAlfabet = alfabet + alfabet.ToLower();
+            var letterQty = fullAlfabet.Length;
+            var retVal = "";
+            for (int i = 0; i < request.PlainText.Length; i++)
+            {
+                var c = request.PlainText[i];
+                var index = fullAlfabet.IndexOf(c);
+                if (index < 0)
+                {
+                    //если символ не найден, то добавляем его в неизменном виде
+                    retVal += c.ToString();
+                }
+                else
+                {
+                    var codeIndex = (letterQty + index + request.Key) % letterQty;
+                    retVal += fullAlfabet[codeIndex];
+                }
+            }
+
+            return Task.FromResult(new EncryptText
+            {
+                Response = "Зашифрованый текст " + retVal
+            });
+        }
+    }
+}
